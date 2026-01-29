081f0713a70a0f245822bde092f6787d5c6e8693
okdol1 <okdol0611@gmail.com>
2026-01-30 08:50:39 +0900

    chore: update routing and i18n for new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
-import { Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import PostDetail from './pages/PostDetail';
-import NotFound from './pages/NotFound';
+import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import PostDetail from "./pages/PostDetail";
+import BookPage from "./pages/Book";
+import BookDetail from "./pages/BookDetail";
+import Playlist from "./pages/Playlist";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -13,8 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
           <Route path="about" element={<About />} />
           <Route path="post/:id" element={<PostDetail />} />
+          <Route path="book" element={<BookPage />} />
+          <Route path="book/:id" element={<BookDetail />} />
+          <Route path="playlist" element={<Playlist />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
